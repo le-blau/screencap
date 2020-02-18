0fa7d43f2b7b561aa3406b8beb9212bbd12f1dd6
blau <>
2020-02-18 22:34:22 +0900

    コメントを追記

--- screencap.py
+++ screencap.py
@@ -15,9 +15,11 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 file_name = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + '.png'
 
 check_input()
+# 第1引数に保存先はディレクトリを指定
 save_path = sys.argv[1] + '/' + 'ScreenCapture_' + today
 
 if not os.path.exists(save_path):
     os.mkdir(save_path)
 
+# キャプチャ
 ImageGrab.grab().save(save_path + '/' + file_name)
